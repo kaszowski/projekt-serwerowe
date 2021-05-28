1d59b2040d7f98f1cd6f7e14098d5f921613803b
kaszowski <michal.kaszowski@gmail.com>
2021-05-28 12:15:00 +0200

    commit Kaszowski 1.0.0

---
+++ server.js
@@ -0,0 +1,72 @@
+var express = require("express")
+var MongoClient = require('mongodb').MongoClient;
+var path = require("path")
+const bodyParser = require('body-parser');
+const http = require('http');
+const { Server } = require("socket.io");
+
+var app = express()
+
+var PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = new Server(server);
+var url = "mongodb://localhost:27017/test";
+
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+//wysyłanie pliku
+app.get("/", function(req, res){
+    
+    res.sendFile("/public/index.html")
+})
+
+//tu wysyłanie danych z mongo
+app.post("/data", function(req, res){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("test")
+        dbo.collection("TestCol").findOne({level: parseInt(req.body.level)},function(err, result){
+            res.send(result.content)
+            db.close();
+        })
+    });
+})
+
+//obsługa websocketów - biblioteka socket.io
+var rooms = [[]]
+io.on('connection', (socket) => {
+    console.log("connection")
+    //tutaj jest przydzielanie do roomów
+    //każdy pokój to 2 graczy
+    socket.on("login", (arg)=>{
+        console.log("login")
+        switch(rooms[rooms.length-1].length)
+        {
+            case 0:
+                {
+                    rooms[rooms.length-1][0] = socket.id
+                    break;
+                }
+            case 1:
+                {
+                    rooms[rooms.length-1][1] = socket.id
+                    break;
+                }
+            case 2:
+                {
+                    rooms.push([])
+                    rooms[rooms.length-1][0] = socket.id
+                    break;
+                }    
+        }
+        socket.join(rooms.length-1)
+        socket.emit("joined", "user "+socket.id+" joined room "+(rooms.length-1))
+    })
+});  
+
+
+server.listen(PORT, function () { 
+    console.log("start serwera na porcie " + PORT )
+})
