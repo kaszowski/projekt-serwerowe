e4cf1941e17b41ca2e26e05d44b8f4087c97ba41
JakubH <homarofficial@gmail.com>
2021-06-12 20:57:51 +0200

    updateMovement Fix v5

--- server.js
+++ server.js
@@ -17,17 +17,17 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 //wysyłanie pliku
-app.get("/", function(req, res){
-    
+app.get("/", function (req, res) {
+
     res.sendFile("/public/index.html")
 })
 
 //tu wysyłanie danych z mongo
-app.post("/data", function(req, res){
-    MongoClient.connect(url, function(err, db) {
+app.post("/data", function (req, res) {
+    MongoClient.connect(url, function (err, db) {
         if (err) throw err;
         var dbo = db.db("test")
-        dbo.collection("TestCol").findOne({level: parseInt(req.body.level)},function(err, result){
+        dbo.collection("TestCol").findOne({ level: parseInt(req.body.level) }, function (err, result) {
             res.send(result.content)
             db.close();
         })
@@ -40,43 +40,40 @@ io.on('connection', (socket) => {
     console.log("connection")
     //tutaj jest przydzielanie do roomów
     //każdy pokój to 2 graczy
-    socket.on("login", (arg)=>{
+    socket.on("login", (arg) => {
         console.log("login")
-        switch(rooms[rooms.length-1].length)
-        {
+        switch (rooms[rooms.length - 1].length) {
             case 0:
                 {
-                    rooms[rooms.length-1][0] = socket.id
+                    rooms[rooms.length - 1][0] = socket.id
                     break;
                 }
             case 1:
                 {
-                    rooms[rooms.length-1][1] = socket.id
+                    rooms[rooms.length - 1][1] = socket.id
                     break;
                 }
             case 2:
                 {
                     rooms.push([])
-                    rooms[rooms.length-1][0] = socket.id
+                    rooms[rooms.length - 1][0] = socket.id
                     break;
-                }    
+                }
         }
-        socket.join(rooms.length-1)
-        io.to(rooms.length-1).emit("joined", "user "+socket.id+" joined room "+(rooms.length-1))
+        socket.join(rooms.length - 1)
+        io.to(rooms.length - 1).emit("joined", JSON.stringify({ message: "user " + socket.id + " joined room " + (rooms.length - 1), playerType: rooms[rooms.length - 1].length }))
     })
-        socket.on("updateMovement", (arg)=>{
-        for(var i=0; i<rooms.length; i++)
-        {
-            if(rooms[i][0]==socket.id || rooms[i][1]==socket.id)
-            {
+    socket.on("updateMovement", (arg) => {
+        for (var i = 0; i < rooms.length; i++) {
+            if (rooms[i][0] == socket.id || rooms[i][1] == socket.id) {
                 io.to(i).emit("updateMovement", arg)
                 break
             }
         }
     })
-});  
+});
 
 
-server.listen(PORT, function () { 
-    console.log("start serwera na porcie " + PORT )
+server.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
 })
